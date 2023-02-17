b4887e724ae544f5ab87f34d67975d8be5c61383
TYLER BENNETT <tyler.bennett52@gmail.com>
2023-02-17 11:22:21 -0600

    modularized routes

--- src/server.js
+++ src/server.js
@@ -8,36 +8,14 @@ const bcrypt = require('bcrypt');
 const { Sequelize, DataTypes } = require('sequelize');
 const basicAuth = require('./auth/middleware/basic')
 const { userModel } = require('./auth/models')
+const router = require('./auth/router');
 
 const PORT = process.env.PORT || 3002;
 const app = express();
 
 app.use(express.json());
-
-// allows us to accept webform data.  aka process FORM input and add to request body
 app.use(express.urlencoded({extended: true}));
-
-
-
-app.post('/signup', async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-    const encryptedPassword = await bcrypt.hash(password, 5);
-    console.log(username, encryptedPassword);
-    let newUser = await userModel.create({
-      username, 
-      password: encryptedPassword,
-    });
-    res.status(200).send(newUser);
-  } catch (e){
-    next(e);
-  }
-});
-
-// starter code will different
-app.post('/signin', basicAuth, (req, res, next) => {
-  res.status(200).send(req.user);
-});
+app.use(router);
 
 
 module.exports = { app, PORT }
